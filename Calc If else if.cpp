1d96e171333975be36d11d68dd813a3b57f3f862
Unionc0de <144795080+Unionc0de@users.noreply.github.com>
2023-09-19 14:41:16 +0300

    Add files via upload

---
+++ Calc If else if.cpp
@@ -0,0 +1,49 @@
+﻿
+
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	float a,b ; 
+	char c;
+	cout << "Введите числа\n";
+	cin >> a;
+	cin >> b;
+	cout << "Введите действие:\n + - Сложение \n - - Вычитание \n * - Умножение \n / - Деление\n";
+	cin >> c;
+	
+	if (c == '+') {
+		cout << "Результат: \n";
+		cout << a + b;
+	}
+
+	else if (c == '-') {
+		cout << "Результат: \n";
+		cout << a - b;
+	}
+
+	else if (c == '*') {
+		cout << "Результат: \n";
+		cout << a * b;
+	}
+
+	else if  (c == '/') 
+	{
+		if (b == 0) {
+			cout << "На ноль делить нельзя";
+		}
+		else {
+			cout << "Результат: \n";
+			cout << a / b;
+		}
+		
+	}
+}
+
+
+
+
+
+
