1fa9fd0ae287150340651a900d7f1b71ee2236b4
Unionc0de <144795080+Unionc0de@users.noreply.github.com>
2024-01-31 10:15:11 +0300

    Update copyDunamicStack.cpp

--- copyDunamicStack.cpp
+++ copyDunamicStack.cpp
@@ -1,32 +1,6 @@
 ﻿#include <iostream>
 using namespace std;
 
-//int main()
-//{
-//	setlocale(LC_ALL, "rus");
-//	srand(time(NULL));
-//	const int SIZE = 10;
-//	int* array = new int[10]{ 1,2,3,4,5,6,7,8,9,0 };
-//	cout << "До добавления - " << endl;
-//	for (int i = 0; i < 10; i++) {
-//		cout << array[i] << " ";
-//	}
-//	int* buf = new int[SIZE + 1];
-//
-//	for (int i = 0; i < SIZE; i++) {
-//		buf[i] = array[i];
-//	}
-//	buf[SIZE] = 99;
-//
-//	delete[] array;
-//	array = buf;
-//	cout << endl <<"после добавления - " << endl;
-//	for (int i = 0; i < SIZE + 1; i++) {
-//		cout << array[i] << " ";
-//	}
-//}
-
-
 void showarr(int* arr) {
 	int size = _msize(arr) / sizeof(arr[0]);
 	for (int i = 0; i < size; i++) {
